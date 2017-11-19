a6cf9aa1607137b2433927a5e87d05e53808f801
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2017-11-19 18:40:39 -0500

    Update GUI2.java

--- GUI2.java
+++ GUI2.java
@@ -5,43 +5,43 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-
 public class GUI2 {
-	private JFrame frame;
-	private JTextField numberField;
-	private JLabel validLabel;
-	private JButton verifyButton;
+	private JFrame Frame;
+	private JTextField number;
+	private JLabel valid;
+	private JButton verify;
 	public GUI2(){
-		numberField=new JTextField(16);
-		validLabel=new JLabel("not verified");
-		verifyButton=new JButton("Verify Employee Number");
-		frame=new JFrame("Employee number verifier");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(new Dimension(350,100));
-		frame.setLayout(new FlowLayout());
-		frame.add(numberField);
-		frame.add(verifyButton);
-		frame.add(validLabel);
-		frame.setVisible(true);
+		number=new JTextField(16);
+		valid=new JLabel("not verified");
+		verify=new JButton("Verify Employee Number");
+		Frame=new JFrame("Employee number verifier");
+		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Frame.setSize(new Dimension(350,100));
+		Frame.setLayout(new FlowLayout());
+		Frame.add(number);
+		Frame.add(verify);
+		Frame.add(valid);
+		Frame.setVisible(true);
 	}
-	public boolean Confirm(String text){
-		int sum=0;
-		for(int i=text.length()-1;i>=0;i--){
-			int digit=Integer.parseInt(text.substring(i, i+1));
-			if(i%2==0){
-				digit*=2;
-			}
-			sum+=(digit/10)+(digit%10);
-		}
-		return sum%10==0&&text.startsWith("4");
-	}
-	public void actionPerformed(ActionEvent event){
-		String text=numberField.getText();
-		if(Confirm(text)){
-			validLabel.setText("Valid number!");
+	public void actionPerformed(ActionEvent Event){
+		String word=number.getText();
+		if(check(word)){
+			valid.setText("This is a valid number!");
 		}else{
-			validLabel.setText("Invalid number!");
+			valid.setText("This is a invalid number!");
 		}
+	}
+	public boolean check(String word){
+		int add=0;
+		int i,digit;
+		for(i=word.length()-1;i>-1;i--){
+			digit=Integer.parseInt(word.substring(i, i+1));
+			if(i%2==0){
+				(digit*=2);
+			}
+			add+=((digit/10)+(digit%10));
+		}
+		return add%10==0 && word.startsWith("4");
 	}
 	public static void main (String[] args){
 		GUI2 gui=new GUI2();							
