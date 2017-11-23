4341f47ecd82fea9f190b0503581390911a1140e
Neelpatel145 <33728003+Neelpatel145@users.noreply.github.com>
2017-11-22 19:19:02 -0500

    Add files via upload

--- GUIPrintEmployees.java
+++ GUIPrintEmployees.java
@@ -5,42 +5,19 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Collections;
 public class GUIPrintEmployees extends JFrame implements ActionListener {
-	    private ArrayList<Employee> list;
-	    private GUIPrintEmployees view;
+	    private ArrayList<Employee> model;
+	    private GUIPrintEmployees parentview;
 	    private Container content;
-	    private JPanel Title;
-	    private JRadioButton employee;
-	    private JTextField name, title, salary;
-	    private JButton create;
-	    private JPanel Design;
-	    private JLabel header;
+	    private JPanel form;
+	    private JLabel heading;
 	    private ButtonGroup employeetype;
-	    public void Employee() {
-	        try {
-	            Employee New;
-	            String name = name.getText();
-	            double salary = Double.parseDouble(salary.getText() );
-	            String selected = employeetype.getSelection().getActionCommand();
-	            if (selected == "employee") {
-	                String department = department.getText();
-	                New = new Employee(name,salary,department);
-	            }
-	            list.add(New);
-	            Painter.repaint(view);
-	            heading.setText("Employee was added.");
-	        }
-	        catch (Exception ex) {
-	            heading.setText("Employee was not added to list. ");
-	        }
-	    } 
-	    public void Button() {
-	        create = new JButton("Create employee");
-	        create.addActionListener(this);
-	        Design.add(create);
-	    }
-	    public GUIPrintEmployee(ArrayList<Employee> model, EmployeeListView view) {
-	        model=model;
-	        view=view;
+	    private JPanel titlepanel;
+	    private JRadioButton worker;
+	    private JTextField name,title,salary;
+	    private JButton create;
+	    public GUIPrintEmployee(ArrayList<Employee> model, EmployeeListView parentview) {
+	        model = model;
+	        parentview = parentview;
 	        GUI();
 	        Name();
 	        Title();
@@ -48,41 +25,66 @@ public class GUIPrintEmployees extends JFrame implements ActionListener {
 	        Button();
 	    }
 	    public void GUI() {
-	        content=getContentPane();
+	        content = getContentPane();
 	        content.setLayout(new FlowLayout());
 	        setTitle("Add Employees");
 	        setSize(500, 500);
 	        setVisible(true);
 	        setSize(200, 100);
-	        Design=new JPanel();
-	        Design.setLayout(new BoxLayout(form, BoxLayout.Y_AXIS));
+	        form = new JPanel();
+	        form.setLayout(new BoxLayout(form, BoxLayout.Y_AXIS));
 	        content.add(form);
 	    }
 	    public void Name() {
 	        JPanel panel = new JPanel();
 	        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-	        panel.add(new JLabel("Name:b", JLabel.LEFT));
+	        panel.add(new JLabel("Name:", JLabel.LEFT));
 	        name = new JTextField();
 	        name.setColumns(10);
 	        panel.add(name);
-	        Design.add(panel);
+	        form.add(panel);
 	    }
 	    public void Title() {
-	        Title=new JPanel();
-	        Title.setLayout(new BoxLayout(titlepanel, BoxLayout.X_AXIS));
-	        Title.add(new JLabel("Title: ", JLabel.LEFT));
-	        title=new JTextField();
-	        title.setColumns(10);
-	        Title.add(title);
-	        Design.add(Title);
+	        titlepanel = new JPanel();
+	        titlepanel.setLayout(new BoxLayout(titlepanel, BoxLayout.X_AXIS));
+	        titlepanel.add(new JLabel("Title:", JLabel.LEFT));
+	        title = new JTextField();
+	        title.setColumns(15);
+	        titlepanel.add(title);
+	        form.add(titlepanel);
 	    }
 	    public void Salary() {
 	        JPanel panel = new JPanel();
 	        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-	        panel.add(new JLabel("Salary: ", JLabel.LEFT));
+	        panel.add(new JLabel("Salary:", JLabel.LEFT));
 	        salary = new JTextField();
-	        salary.setColumns(10);
+	        salary.setColumns(15);
 	        panel.add(salary);
-	        Design.add(panel); 
+	        form.add(panel); 
+	    }
+	    public void Button() {
+	        create = new JButton("Create employee");
+	        create.addActionListener(this);
+	        form.add(create);
+	    }
+	    
+public void Employee() {
+	        try {
+	            Employee New;
+	            String name = name.getText();
+	            double salary = Double.parseDouble(salary.getText() );
+	            String selected = employeetype.getSelection().getActionCommand();
+	            if (selected == "worker") {
+	                String department = department.getText();
+	                New = new Employee(name,  salary, department);
+	            }
+	            model.add(New);
+	            Painter.repaint(parentview);
+	            heading.setText("Employee was added.");
+	        }
+	        catch (Exception ex) {
+	            heading.setText("Employee was not added to list. ");
+	        }
 	    }
 	}
+
